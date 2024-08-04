5bc17831ae895707c9ad433f9dfffe81d205f5aa
anton <antonkuzmn1@gmail.com>
2024-08-05 04:12:51 +0700

    added welcome page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
-import {Controller, Get} from '@nestjs/common';
-import {AppService} from './app.service';
+import {Controller, Get, Res} from '@nestjs/common';
+import {Response} from 'express';
+import {join} from 'path';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
-    }
 
     @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    getHello(@Res() res: Response) {
+        res.sendFile(join(__dirname, 'public', 'index.html'));
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,16 @@ limitations under the License.
 
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {join} from 'path';
+
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+
     await app.listen(3000);
 }
 
-bootstrap();
+bootstrap().then();

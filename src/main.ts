8c488e795d8aa7ce903cab4804344cd1ab31f392
anton <antonkuzmn1@gmail.com>
2024-08-05 04:08:37 +0700

    autoformatting

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,12 @@ limitations under the License.
  */
 
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
+
 bootstrap();
